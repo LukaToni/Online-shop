7e787149dfdad8caf25ebbea06f9d00985e90b68
PascalSavage <terminator106@gmail.com>
2017-12-20 12:12:14 +0100

    skripta za bazo

---
+++ spletnaTrgovina.sql
@@ -0,0 +1,90 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema spletna
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema spletna
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `spletna` DEFAULT CHARACTER SET utf8 ;
+USE `spletna` ;
+
+-- -----------------------------------------------------
+-- Table `spletna`.`Administrator`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `spletna`.`Administrator` (
+  `ime` VARCHAR(45) NOT NULL,
+  `priimek` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `geslo` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`email`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `spletna`.`Prodajalec`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `spletna`.`Prodajalec` (
+  `ime` VARCHAR(45) NOT NULL,
+  `priimek` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `geslo` VARCHAR(45) NOT NULL,
+  `aktiviran_racun` boolean NOT NULL,
+  PRIMARY KEY (`email`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `spletna`.`Stranka`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `spletna`.`Stranka` (
+  `ime` VARCHAR(45) NOT NULL,
+  `priimek` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `geslo` VARCHAR(45) NOT NULL,
+  `naslov` VARCHAR(45) NOT NULL,
+  `telefonska_st` VARCHAR(45) NOT NULL,
+  `aktiviran_racun` boolean NOT NULL,
+  PRIMARY KEY (`email`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `spletna`.`AnonimniOdjemalec`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `spletna`.`AnonimniOdjemalec` (
+  `ip` VARCHAR(45) NOT NULL,
+  `cas_dostopa` DATETIME NOT NULL,
+  PRIMARY KEY (`ip`, `cas_dostopa`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `spletna`.`Artikel`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `spletna`.`Artikel` (
+  `ime` VARCHAR(45) NOT NULL,
+  `cena` FLOAT NOT NULL,
+  `aktiviran` boolean NOT NULL,
+  PRIMARY KEY (`ime`, `cena`))
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `spletna`.`Dnevnik`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `spletna`.`Dnevnik` (
+  `vloga` VARCHAR(45) NOT NULL,
+  `datum` DATETIME NOT NULL,
+  `akcija` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`vloga`, `datum`, `akcija`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
