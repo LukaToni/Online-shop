d80ea3df52736100095be568de2a946f4f8b7fef
Luka Toni <luka.toni93@gmail.com>
2018-01-24 12:24:03 +0100

    Added ReCaptcha for registration form

--- verzija1/register.php
+++ verzija1/register.php
@@ -3,61 +3,87 @@ session_start();
 
 unset($_SESSION['register_success']);
 
-if(isset($_POST['registration_submitted'])){
-    include 'database/DB_Engine.php';
-    $hash = hash('sha256', $_POST['password']."greensalt");
-    $values = "(NULL,'".$_POST['email']."','".$hash."', 4)";
-    $rez = executeQuery("INSERT INTO users (id, email, password, role_id) "
-            . "VALUES ".$values);
-    
-    if($rez == 1){
-        $_SESSION['register_success'] = true;
-    }
-    
-}
-        
+
+// grab recaptcha library
+// https://webdesign.tutsplus.com/tutorials/how-to-integrate-no-captcha-recaptcha-in-your-website--cms-23024
+require_once "recaptchalib.php";
+// your secret key
+$secret = "6Lc5I0IUAAAAAGlaae1FBhp51cJ1fVPHwske-MA1";
+
+// empty response
+$resp = null;
+
+$reCaptcha = new ReCaptcha($secret);
+
 ?>
 
 <html>
-    <?php include 'templates/template_head.php'; ?>
-
+<?php include 'templates/template_head.php'; ?>
     <body>
-        <div class="title" id="title" style="height: 50px; text-align: center"><h1>Registration page</h1></div>
-        <?php        include 'templates/template_navigation_bar.php'; ?>
-        
-        <div class="context" id="context">
-            
-             <?php if(!isset($_SESSION['register_success'])) { ?>
+    <div class="title" id="title" style="height: 50px; text-align: center"><h1>Registration page</h1></div>
+    <?php include 'templates/template_navigation_bar.php'; ?>
+
+    <div class="context" id="context">
+
+        <?php if(!isset($_SESSION['register_success'])) { ?>
             <!-- REGISTRATION -->
             <div class="registration" id="registration" align = "center">
-                <div style = "width:300px; border: solid 1px #333333; margin: 25px; " align = "center">
+                <div style = "width:360px; border: solid 1px #333333; margin: 25px; " align = "center">
                     <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Registration</b></div>
                     <div style = "margin:30px">
+
                         <form action = "" method = "post">
                             <label>Email:</label><br/>
                             <input type = "text" name = "email" class = "box"/><br/><br/>
                             <label>Password:</label><br/>
                             <input type = "password" name = "password" class = "box" /><br/><br/>
+                            <!-- RECAPTCHA -->
+                            <div class="g-recaptcha" data-sitekey="6Lc5I0IUAAAAAFoTSPEQg_-AQAELenL70_G4PhDJ" data-theme="dark"></div><br/>
                             <input type = "submit" name="registration_submitted" value = " Submit "/><br/>
                         </form>
                         <div style = "font-size:11px; color:#cc0000; margin-top:10px">
-                            <?php 
+                            <?php
                             if(isset($_SESSION['error_msg_registration'])){
                                 echo $_SESSION['error_msg_registration'];
-                            } 
+                            }
+
+                            if (!empty($_POST)) {
+                                // if submitted check response
+                                if ($_POST["g-recaptcha-response"] && $_POST["email"]) {
+                                    $resp = $reCaptcha->verifyResponse($_SERVER["REMOTE_ADDR"], $_POST["g-recaptcha-response"]);
+                                    if ($resp != null) {
+                                        echo "Hi " . $_POST["email"] . ", thanks for submitting the form!";
+
+                                        include 'database/DB_Engine.php';
+                                        $hash = hash('sha256', $_POST['password']."greensalt");
+                                        $values = "(NULL,'".$_POST['email']."','".$hash."', 4)";
+                                        $rez = executeQuery("INSERT INTO users (id, email, password, role_id) "
+                                            . "VALUES ".$values);
+
+                                        if($rez == 1 ){
+                                            $_SESSION['register_success'] = true;
+                                            header("Location:login.php");
+                                        }
+
+                                    } else {
+                                        $errors = $resp->getErrorCodes();
+                                    }
+                                } else {
+                                    echo '<p>You did not fill all form!</p>';
+                                }
+                            }
                             ?>
                         </div>
                     </div>
                 </div>
             </div>
-             <?php }else{
-                 echo "Registration success";
-                 
-                    } ?>
+        <?php }else{
+            echo "Registration success";
+        } ?>
+    </div>
 
-
-        </div>
-
-        <?php include 'templates/template_footer.php'; ?>
+    <?php include 'templates/template_footer.php'; ?>
+    <!-- js for captcha -->
+    <script src='https://www.google.com/recaptcha/api.js'></script>
     </body>
 </html>
--- verzija1/templates/template_head.php
+++ verzija1/templates/template_head.php
@@ -1,7 +1,10 @@
 <head>
-        <link href="css/styles.css" rel="stylesheet" >
-        <!--<link href="https://use.fontawesome.com/releases/v5.0.2/css/all.css" rel="stylesheet">-->
-        <!--core first + styles last-->
-        <link href="fontawesome-free-5.0.2/web-fonts-with-css/css/fontawesome-all.css" rel="stylesheet">
-        <script src="js/script.js"></script>
-    </head>
+    <link href="css/styles.css" rel="stylesheet" >
+    <!--<link href="https://use.fontawesome.com/releases/v5.0.2/css/all.css" rel="stylesheet">-->
+    <!--core first + styles last-->
+    <link href="fontawesome-free-5.0.2/web-fonts-with-css/css/fontawesome-all.css" rel="stylesheet">
+    <script src="js/script.js"></script>
+    <!--favicons-->
+    <link href="https://static.tutsplus.com/assets/favicon-3ef7b4f54ed4378b31ff3124d3c4bff8.png" rel="shortcut icon" type="image/png" />
+    <link href="https://static.tutsplus.com/assets/apple-touch-icon-3ef7b4f54ed4378b31ff3124d3c4bff8.png" rel="apple-touch-icon" type="image/png" />
+</head>
