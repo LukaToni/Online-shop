c18066e4208ad8b81294fa35fe5c278f0642d62e
Luka Toni <luka.toni93@gmail.com>
2018-02-03 17:37:46 +0100

    Order - in progress and history front end complete

--- verzija1/orders.php
+++ verzija1/orders.php
@@ -7,9 +7,9 @@ if(!isset($_SESSION['logged_in'])){
 include 'database/DB_Engine.php';
 
 if($_SERVER['REQUEST_METHOD'] == "POST"){
-    
+
     if(isset($_POST['confirm'])){
-    executeQuery("UPDATE orders SET state = 'confirmed' WHERE order_id = '".$_POST['order_id']."'");
+        executeQuery("UPDATE orders SET state = 'confirmed' WHERE order_id = '".$_POST['order_id']."'");
     }else if($_POST['cancel']){
         executeQuery("UPDATE orders SET state = 'canceled' WHERE order_id = '".$_POST['order_id']."'");
     }
@@ -18,62 +18,66 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 ?>
 
 <html>
-    <?php include 'templates/template_head.php'; ?>
-    <body>
-        <div class="title" id="title" style="height: 50px; text-align: center"><h1>Orders</h1></div>
-        <?php   include 'templates/template_navigation_bar.php'; ?>
+<?php include 'templates/template_head.php'; ?>
+<body>
+<div class="title" id="title" style="height: 50px; text-align: center"><h1>ORDERS</h1></div>
+<?php   include 'templates/template_navigation_bar.php'; ?>
 
-        <div class="context" id="context">
-            <?php if($_SERVER['QUERY_STRING'] != "history"){ ?>
-            <table>
-                
-                <?php
+<div class="context" id="context">
+    <div class="orders" id="orders" align = "center" style="margin-top: 25px">
+        <div style = "border: solid 1px #333333;" align = "center">
+            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Orders</b></div>
+            <div style = "margin:30px">
+                <?php if($_SERVER['QUERY_STRING'] != "history"){ ?>
+                <table>
+
+                    <?php
                     if ($_SESSION['role_id'] == 2){
-                    $rez = fetchRows("SELECT distinct(order_id) FROM orders WHERE state = 'pending'");
+                        $rez = fetchRows("SELECT distinct(order_id) FROM orders WHERE state = 'pending'");
 
-                    foreach($rez as $row){
-                ?>
-                <form method="post" action ="">
-                    <input type="hidden" name="order_id" value="<?php echo $row['order_id']; ?>" />
-                <tr>
-                <td><a href="view_order?order_id=<?php echo $row['order_id']; ?>">#<?php echo $row['order_id']; ?></a></td>
-                <td><input type="submit" name="confirm" value="Confirm"/><input type="submit" name="cancel" value="Cancel"/></td>
-                </tr>
-                </form>
-                <?php }
-                
-            }else{
-                $rez = fetchRows("SELECT distinct(order_id), state FROM orders WHERE user_id = ".$_SESSION['user_id']);
-                foreach($rez as $row){ ?>
-                <tr>
-                <td><a href="view_order?order_id=<?php echo $row['order_id']; ?>">#<?php echo $row['order_id']; ?></a></td>
+                        foreach($rez as $row){
+                            ?>
+                            <form method="post" action ="">
+                                <input type="hidden" name="order_id" value="<?php echo $row['order_id']; ?>" />
+                                <tr>
+                                    <td><a href="view_order?order_id=<?php echo $row['order_id']; ?>"><?php echo $row['order_id']; ?></a></td>
+                                    <td><input type="submit" name="confirm" value="Confirm"/><input type="submit" name="cancel" value="Cancel"/></td>
+                                </tr>
+                            </form>
+                        <?php }
+
+                    }else{
+                        $rez = fetchRows("SELECT distinct(order_id), state FROM orders WHERE user_id = ".$_SESSION['user_id']);
+                        foreach($rez as $row){ ?>
+                            <tr>
+                                <td><a href="view_order?order_id=<?php echo $row['order_id']; ?>"><?php echo $row['order_id']; ?></a></td>
+                                <td><?php echo $row['state'] ?></td>
+                            </tr>
+
+                        <?php }
+                    }
+                    ?>
+
+
+                </table></div></div></div>
+    <?php }else{
+        echo "<table>";
+        $rez = fetchRows("SELECT distinct(order_id), state FROM orders WHERE state <> 'pending'");
+
+        foreach($rez as $row){ ?>
+
+            <input type="hidden" name="order_id" value="<?php echo $row['order_id']; ?>" />
+            <tr>
+                <td><a href="view_order?order_id=<?php echo $row['order_id']; ?>"><?php echo $row['order_id']; ?></a></td>
                 <td><?php echo $row['state'] ?></td>
-                </tr>
-                
-                <?php }
-            }
-                ?>
-                
-                
-            </table>
-            <?php }else{
-                echo "<table>";
-                $rez = fetchRows("SELECT distinct(order_id), state FROM orders WHERE state <> 'pending'");
-                
-                foreach($rez as $row){ ?>
-               
-                    <input type="hidden" name="order_id" value="<?php echo $row['order_id']; ?>" />
-                <tr>
-                <td><a href="view_order?order_id=<?php echo $row['order_id']; ?>">#<?php echo $row['order_id']; ?></a></td>
-                <td><?php echo $row['state'] ?></td>
-                </tr>
-                <?php 
-                }
-            }
-            echo "</table>"; ?>
+            </tr>
+            <?php
+        }
+    }
+    echo "</table>"; ?>
 
-        </div>
+</div>
 
-        <?php include 'templates/template_footer.php'; ?>
-    </body>
+<?php include 'templates/template_footer.php'; ?>
+</body>
 </html>
--- verzija1/view_order.php
+++ verzija1/view_order.php
@@ -1,27 +1,104 @@
 <?php
-    session_start();
-    include 'database/DB_engine.php';
+session_start();
+include 'database/DB_engine.php';
 ?>
 
 <html>
-    <?php include 'templates/template_head.php'; ?>
-    <body>
-        <div class="title" id="title" style="height: 50px; text-align: center"><h1>INDEX</h1></div>
-        <?php  include 'templates/template_navigation_bar.php'; ?>
-        <div class="context" id="context">
+<?php include 'templates/template_head.php'; ?>
+<link rel='stylesheet' type='text/css' href='invoice/css/style.css' />
+<link rel='stylesheet' type='text/css' href='invoice/css/print.css' media="print" />
+<script type='text/javascript' src='invoice/js/jquery-1.3.2.min.js'></script>
+<script type='text/javascript' src='invoice/js/example.js'></script>
+<body style="display: block; margin: 8px;">
+<div class="title" id="title" style="height: 50px; text-align: center"><h1>VIEW ORDER</h1></div>
+<?php  include 'templates/template_navigation_bar.php'; ?>
+<div class="context" id="context">
+    <div class="view_orders" id="view_orders" align = "center" style="margin-top: 25px">
+        <div style = "border: solid 1px #333333;" align = "center">
+            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>View order</b></div>
+            <div style = "margin:30px">
 
-        <?php
-        #confirm order
-        $rez = fetchRows("SELECT name, quantity FROM orders, articles WHERE articles.id = orders.article_id AND order_id = '".$_GET['order_id']."'");
-        #$total = 0;
-        foreach($rez as $row){
-            #$total += $row['price']*$row['quantity'];
-            echo "<p>".$row['name']." ".$row['quantity']."x</p>";
-        }
-        #echo "<p>Total: ".$total."€</p>";
-        ?>
+                <?php
 
+
+                echo"<div id='page-wrap'>".
+                    "<div id='inside-print'>".
+                    "<p id='header'>INVOICE</p>".
+                    "<div id='identity'>".
+                    "<p id='address'>EP Team<br />".
+                    "1223 Appleseed Street<br />".
+                    "1000 Ljubljana<br />".
+                    "<br />".
+                    "Phone: (555) 555-5555</p>".
+                    "</div>".
+
+                    "<div style='clear:both'></div>".
+
+                    "<div id='customer''>".
+
+                    "<p id='customer-title'></p>".
+
+                    "<table id='meta'>".
+                    "<!--<tr>".
+                    "<td class='meta-head''>Invoice #</td>".
+                    "<td>000123</td>".
+                    "</tr>-->".
+                    "<tr>".
+                    "<td class='meta-head'>Date</td>".
+                    "<td id='date''>December 15, 2009</td>".
+                    "</tr>".
+                    "</table>".
+                    "</div>".
+
+                    "<table id='items'>".
+                    "<tr>".
+                    "<th>Item</th>".
+                    "<th>Unit Cost</th>".
+                    "<th>Quantity</th>".
+                    "<th>Price</th>".
+                    "</tr>";
+
+                #confirm order
+                $rez = fetchRows("SELECT name, quantity, price FROM orders, articles WHERE articles.id = orders.article_id AND order_id = '".$_GET['order_id']."'");
+                $total = 0;
+
+                foreach($rez as $row){
+                    $oneItem = $row['price']*$row['quantity'];
+
+                    echo "<tr class='item-row'>".
+                        "<td class='item-name'><div>".$row['name']."</div></td>".
+                        "<td class='cost'>".$row['price']."<p style='display: inline;'> €</p></td>".
+                        "<td class='qty'>".$row['quantity']."</td>".
+                        "<td><span class='price'>".$oneItem."<p style='display: inline;'> €</p></span></td>".
+                        "</tr>";
+
+                    $total += $oneItem;
+                }
+                echo  "<tr>".
+                    "<td class='blank'> </td>".
+                    "<td class='blank'> </td>".
+                    "<td style='text-align: right'>Total</td>".
+                    "<td ><div id='total'>".$total."<p style='display: inline;'> €</p></div></td>".
+                    "</tr>".
+                    "</table>".
+
+                    "<div id='terms'>".
+                    "<h5>Terms</h5>".
+                    "<p>NET 30 Days. Finance Charge of 1.5% will be made on unpaid balances after 30 days.</p>".
+                    "</div><br/>"."</div>".
+                    "<div style='text-align:center'><input type = 'submit' id='print' value = ' Print ' onclick='Popup()'  style='display: inline; width: 20em;  height: 2em;' '/></div><br/>".
+                    "</div>"
+
+                ?>
+
+            </div>
         </div>
-        <?php include 'templates/template_footer.php'; ?>
-    </body>
+    </div>
+</div>
+<?php include 'templates/template_footer.php'; ?>
+<script>
+    var d = new Date();
+    document.getElementById("date").innerHTML = print_today();
+</script>
+</body>
 </html>
