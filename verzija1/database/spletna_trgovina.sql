9c881639dc42c7fc518e121749993721de3e2220
PascalSavage <terminator106@gmail.com>
2018-02-08 13:04:15 +0100

    sample items

--- verzija1/database/spletna_trgovina.sql
+++ verzija1/database/spletna_trgovina.sql
@@ -1,167 +1,202 @@
--- MySQL Workbench Forward Engineering
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: spletna_trgovina
+-- ------------------------------------------------------
+-- Server version	5.7.14
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
+--
+-- Table structure for table `articles`
+--
 
--- -----------------------------------------------------
--- Table `articles`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `articles` ;
+DROP TABLE IF EXISTS `articles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `articles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) COLLATE utf8_slovenian_ci NOT NULL,
+  `price` float NOT NULL,
+  `description` varchar(220) COLLATE utf8_slovenian_ci DEFAULT NULL,
+  `image` mediumblob,
+  `image_type` varchar(45) COLLATE utf8_slovenian_ci DEFAULT NULL,
+  `active` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE IF NOT EXISTS `articles` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `price` FLOAT NOT NULL,
-  `description` VARCHAR(220) NULL,
-  `image` MEDIUMBLOB NULL,
-  `image_type` VARCHAR(45) NULL,
-  `active` TINYINT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+--
+-- Dumping data for table `articles`
+--
 
+LOCK TABLES `articles` WRITE;
+/*!40000 ALTER TABLE `articles` DISABLE KEYS */;
+INSERT INTO `articles` VALUES (1,'Trousers',10,'Blue trousers for adults',NULL,NULL,1),(2,'Shirt',20,'Red shirt for seniors',NULL,NULL,1);
+/*!40000 ALTER TABLE `articles` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Table `roles`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `roles` ;
+--
+-- Table structure for table `articles_rating`
+--
 
-CREATE TABLE IF NOT EXISTS `roles` (
-  `role_id` INT NOT NULL AUTO_INCREMENT,
-  `role_name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`role_id`, `role_name`))
-ENGINE = InnoDB;
+DROP TABLE IF EXISTS `articles_rating`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `articles_rating` (
+  `rating` int(11) NOT NULL,
+  `article_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`rating`,`article_id`,`user_id`),
+  KEY `fk_articles_rating_articles_idx` (`article_id`),
+  KEY `fk_articles_rating_users1_idx` (`user_id`),
+  CONSTRAINT `fk_articles_rating_articles` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_articles_rating_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `articles_rating`
+--
 
--- -----------------------------------------------------
--- Table `users`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `users` ;
+LOCK TABLES `articles_rating` WRITE;
+/*!40000 ALTER TABLE `articles_rating` DISABLE KEYS */;
+/*!40000 ALTER TABLE `articles_rating` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(45) NULL,
-  `last_name` VARCHAR(45) NULL,
-  `street_address` VARCHAR(45) NULL,
-  `city` VARCHAR(45) NULL,
-  `postal_code` VARCHAR(45) NULL,
-  `country` VARCHAR(45) NULL,
-  `phone_number` VARCHAR(45) NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(64) NULL,
-  `active` VARCHAR(45) NULL DEFAULT '1',
-  `role_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `email`),
-  INDEX `fk_users_roles1_idx` (`role_id` ASC),
-  CONSTRAINT `fk_users_roles1`
-    FOREIGN KEY (`role_id`)
-    REFERENCES `roles` (`role_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Table structure for table `orders`
+--
 
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `orders` (
+  `order_id` varchar(32) COLLATE utf8_slovenian_ci NOT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `state` varchar(20) COLLATE utf8_slovenian_ci DEFAULT 'pending',
+  `article_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`order_id`,`article_id`,`user_id`),
+  KEY `fk_orders_articles1_idx` (`article_id`),
+  KEY `fk_orders_users1_idx` (`user_id`),
+  CONSTRAINT `fk_orders_articles1` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_orders_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `articles_rating`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `articles_rating` ;
+--
+-- Dumping data for table `orders`
+--
 
-CREATE TABLE IF NOT EXISTS `articles_rating` (
-  `rating` INT NOT NULL,
-  `article_id` INT NOT NULL,
-  `user_id` INT NOT NULL,
-  PRIMARY KEY (`rating`, `article_id`, `user_id`),
-  INDEX `fk_articles_rating_articles_idx` (`article_id` ASC),
-  INDEX `fk_articles_rating_users1_idx` (`user_id` ASC),
-  CONSTRAINT `fk_articles_rating_articles`
-    FOREIGN KEY (`article_id`)
-    REFERENCES `articles` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_articles_rating_users1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `roles`
+--
 
--- -----------------------------------------------------
--- Table `orders`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `orders` ;
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roles` (
+  `role_id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(45) COLLATE utf8_slovenian_ci NOT NULL,
+  PRIMARY KEY (`role_id`,`role_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE IF NOT EXISTS `orders` (
-  `order_id` VARCHAR(32) NOT NULL,
-  `quantity` INT NULL,
-  `state` VARCHAR(20) NULL DEFAULT 'pending',
-  `article_id` INT NOT NULL,
-  `user_id` INT NOT NULL,
-  PRIMARY KEY (`order_id`, `article_id`, `user_id`),
-  INDEX `fk_orders_articles1_idx` (`article_id` ASC),
-  INDEX `fk_orders_users1_idx` (`user_id` ASC),
-  CONSTRAINT `fk_orders_articles1`
-    FOREIGN KEY (`article_id`)
-    REFERENCES `articles` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_orders_users1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Dumping data for table `roles`
+--
 
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'admin'),(2,'seller'),(3,'client'),(4,'anonymous');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Table `shopping_cart`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `shopping_cart` ;
+--
+-- Table structure for table `shopping_cart`
+--
 
-CREATE TABLE IF NOT EXISTS `shopping_cart` (
-  `serial` INT NOT NULL AUTO_INCREMENT,
-  `article_id` INT NOT NULL,
-  `user_id` INT NOT NULL,
-  PRIMARY KEY (`serial`, `article_id`, `user_id`),
-  INDEX `fk_shopping_cart_users1_idx` (`user_id` ASC),
-  CONSTRAINT `fk_shopping_cart_articles1`
-    FOREIGN KEY (`article_id`)
-    REFERENCES `articles` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_shopping_cart_users1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+DROP TABLE IF EXISTS `shopping_cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `shopping_cart` (
+  `serial` int(11) NOT NULL AUTO_INCREMENT,
+  `article_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`serial`,`article_id`,`user_id`),
+  KEY `fk_shopping_cart_users1_idx` (`user_id`),
+  KEY `fk_shopping_cart_articles1` (`article_id`),
+  CONSTRAINT `fk_shopping_cart_articles1` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_shopping_cart_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `shopping_cart`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+LOCK TABLES `shopping_cart` WRITE;
+/*!40000 ALTER TABLE `shopping_cart` DISABLE KEYS */;
+/*!40000 ALTER TABLE `shopping_cart` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Data for table `roles`
--- -----------------------------------------------------
-START TRANSACTION;
-INSERT INTO `roles` (`role_id`, `role_name`) VALUES (1, 'admin');
-INSERT INTO `roles` (`role_id`, `role_name`) VALUES (2, 'seller');
-INSERT INTO `roles` (`role_id`, `role_name`) VALUES (3, 'client');
-INSERT INTO `roles` (`role_id`, `role_name`) VALUES (4, 'anonymous');
+--
+-- Table structure for table `users`
+--
 
-COMMIT;
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) COLLATE utf8_slovenian_ci DEFAULT NULL,
+  `last_name` varchar(45) COLLATE utf8_slovenian_ci DEFAULT NULL,
+  `street_address` varchar(45) COLLATE utf8_slovenian_ci DEFAULT NULL,
+  `city` varchar(45) COLLATE utf8_slovenian_ci DEFAULT NULL,
+  `postal_code` varchar(45) COLLATE utf8_slovenian_ci DEFAULT NULL,
+  `country` varchar(45) COLLATE utf8_slovenian_ci DEFAULT NULL,
+  `phone_number` varchar(45) COLLATE utf8_slovenian_ci DEFAULT NULL,
+  `email` varchar(45) COLLATE utf8_slovenian_ci NOT NULL,
+  `password` varchar(64) COLLATE utf8_slovenian_ci DEFAULT NULL,
+  `active` varchar(45) COLLATE utf8_slovenian_ci DEFAULT '1',
+  `role_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`email`),
+  KEY `fk_users_roles1_idx` (`role_id`),
+  CONSTRAINT `fk_users_roles1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `users`
+--
 
--- -----------------------------------------------------
--- Data for table `users`
--- -----------------------------------------------------
-START TRANSACTION;
-INSERT INTO `users` (`id`, `first_name`, `last_name`, `street_address`, `city`, `postal_code`, `country`, `phone_number`, `email`, `password`, `active`, `role_id`) VALUES (1, 'Elon', 'Musk', NULL, NULL, NULL, NULL, NULL, 'admin@admin.si', 'f212bd60369e4bf2b09320cab3d2e8b14e13a3a5006ab20e0a39858d560a27ee', '1', 1);
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Elon','Musk',NULL,NULL,NULL,NULL,NULL,'admin@admin.si','f212bd60369e4bf2b09320cab3d2e8b14e13a3a5006ab20e0a39858d560a27ee','1',1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-COMMIT;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
+-- Dump completed on 2018-02-08 13:02:45
